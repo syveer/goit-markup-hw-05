3a912866246dd65cee0193f5fa33cb175f98dbd4
syveer <hanganalexandra@yahoo.com>
2023-07-31 22:02:02 +0300

    hw5

---
+++ js/modal.js
@@ -0,0 +1,14 @@
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const openModalBtn = document.querySelector(".open-button");
+const closeModalBtn = document.querySelector(".close-button");
+const openModal = function () {
+  modal.classList.remove("is-hidden");
+  overlay.classList.remove("is-hidden");
+};
+openModalBtn.addEventListener("click", data-modalopen );
+const closeModal = function () {
+  modal.classList.add("is-hidden");
+  overlay.classList.add("is-hidden");
+};
+closeModalBtn.addEventListener("click", closeModal);
